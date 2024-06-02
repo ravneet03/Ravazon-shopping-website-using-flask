fd175c03c7a87145c3d8cccc82227709b9b6aa14
Ravneet <ravneet@example.com>
2024-06-03 00:45:17 +0530

    first Commit

---
+++ data.py
@@ -0,0 +1,58 @@
+data= [
+    {
+        "id" :  1,
+        "name": 'iphone 15 pro max' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1708678829/Croma%20Assets/Communication/Mobiles/Images/300822_0_vy3iid.png"
+    },
+    {
+        "id" :  2,
+        "name": 'iphone 12 pro max' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://i.ebayimg.com/images/g/vhsAAOSwQW1iuuya/s-l640.jpg"
+    },
+    {
+        "id" :  3,
+        "name": 'vivo v27 pro' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://m.media-amazon.com/images/I/318OQWryxYL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        "id" :  4,
+        "name": 'SAMSUNG Galaxy S24 Ultra 5G' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1705640892/Croma%20Assets/Communication/Mobiles/Images/303817_cc5lmd.png"
+    },
+    {
+        "id" :  5,
+        "name": 'iphone 12 pro max' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://i.ebayimg.com/images/g/vhsAAOSwQW1iuuya/s-l640.jpg"
+    },
+    {
+        "id" :  6,
+        "name": 'iphone 12 pro max' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://i.ebayimg.com/images/g/vhsAAOSwQW1iuuya/s-l640.jpg"
+    },
+    {
+        "id" :  7,
+        "name": 'iphone 12 pro max' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://i.ebayimg.com/images/g/vhsAAOSwQW1iuuya/s-l640.jpg"
+    },
+    {
+        "id" :  8,
+        "name": 'iphone 12 pro max' ,
+        "price" : 150000,
+        "des" : 'this is description',
+        "image" : "https://i.ebayimg.com/images/g/vhsAAOSwQW1iuuya/s-l640.jpg"
+    }
+] 
---
+++ market.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, session, redirect, url_for
+import data
+import time
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Secret key for Flask sessions
+dat = data.data
+
+@app.route("/")
+def homepage():
+    arr = ['home.html', 'product.html']
+    message = session.pop('message', None)  # Get and clear the message from session
+    return render_template(arr, data=dat)
+
+@app.route("/add_to_cart/<int:item_id>")
+def add_to_cart(item_id):
+    item = next((item for item in dat if item['id'] == item_id), None)
+    
+    if item:
+        cart = session.get('cart', [])
+        cart.append(item)
+        session['cart'] = cart
+        session['message'] = f"{item['name']} added to cart"  # Set the message
+        time.sleep(1) 
+        return redirect(url_for('homepage'))
+    else:
+        return "Item not found", 404
+    
+@app.route("/remove_from_cart/<int:item_id>", methods=['POST'])
+def remove_from_cart(item_id):
+    cart = session.get('cart', [])
+    
+    for item in cart:
+        if item['id'] == item_id:
+            cart.remove(item)
+            break
+    
+    session['cart'] = cart
+    return redirect(url_for('cart'))
+
+
+
+@app.route("/cart")
+def cart():
+    cart_items = session.get('cart', [])
+    total_price = sum(item['price'] for item in cart_items)
+    return render_template('cart.html', cart_items=cart_items, total_price=total_price)
+
+if __name__ == "__main__":
+    app.run(debug=True)
